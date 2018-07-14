0c1dfc6d91e72cec116fc3263e7e1da591feadec
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-07-14 12:49:19 -0400

    Day 9 - Introducing Routing and add directive <router-outlet> to the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '../../node_modules/@angular/router';
+import { HrComponent } from './hr/hr.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HrComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot([
+      {path:'hr', component:HrComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
