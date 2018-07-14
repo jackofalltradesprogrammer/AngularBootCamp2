767be7d57f5d87458da8d8cbaa5f26b47181b8bf
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-07-14 18:14:35 -0400

    D9 - Create more components and set up routes to access them directly with url. Also setup PageNoTFound and default one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterModule } from '../../node_modules/@angular/router';
 import { HrComponent } from './hr/hr.component';
+import { HomeComponent } from './home/home.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HrComponent
+    HrComponent,
+    HomeComponent,
+    EmployeeComponent,
+    AboutComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule, RouterModule.forRoot([
-      {path:'hr', component:HrComponent}
+      {path:'hr', component:HrComponent},
+      {path:'home', component:HomeComponent},
+      {path:'employee', component:EmployeeComponent},
+      {path:'about', component:AboutComponent},
+      {path:'', redirectTo: 'home',pathMatch:'full'},
+      {path:'**', component:PageNotFoundComponent}
+
     ])
   ],
   providers: [],
