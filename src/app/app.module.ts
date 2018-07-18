849af488eb7f186ace3710ad1578d6e3422c1963
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-07-18 12:37:50 -0400

    D10 - Add configuration for the Routing(Navigation)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,30 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { DashBoardComponent } from './dash-board/dash-board.component';
+import { RouterModule } from '../../node_modules/@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LogoutComponent } from './logout/logout.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DashBoardComponent,
+    HomeComponent,
+    LogoutComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, FormsModule, RouterModule.forRoot([
+      {path:'', redirectTo:'/home',pathMatch:'full'},
+      {path:'home', component:HomeComponent},
+      {path:'login', component:LoginComponent},
+      {path:'dashboard', component:DashBoardComponent},
+      {path:'logout', component:LogoutComponent},
+      {path:'**', component:PageNotFoundComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
